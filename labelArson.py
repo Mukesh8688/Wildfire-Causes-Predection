6b82032e072f5e36308e7c0a6fe70229cb93d204
Mukesh8688 <55174474+Mukesh8688@users.noreply.github.com>
2020-02-14 12:00:47 -0600

    Add files via upload

---
+++ labelArson.py
@@ -0,0 +1,7 @@
+def set_label_one_arson(reason):
+        cause =0
+        if reason == 'Arson':
+            cause =1
+        else:
+            cause =0
+        return cause 
---
+++ lebelLightning.py
@@ -0,0 +1,7 @@
+def set_label_one_light(reason):
+        cause =0
+        if reason == 'Lightning':
+            cause =1
+        else:
+            cause = 0
+        return cause  
